dbec3acd9efeecdacd2b25b737b2f55dd9ed05b6
reymondgomera <reymondgomera24@gmail.com>
2024-02-19 16:00:48 +0800

    update connection string

--- api/connection.php
+++ api/connection.php
@@ -1,3 +1,3 @@
 <?php
-$connection = mysqli_connect("boredguyscorp.com", "bg-admin", "@BoredGuys", "bgi_cms");
+$connection = mysqli_connect("148.66.138.193", "bg-admin", "@BoredGuys", "bgi_cms");
 ?>
